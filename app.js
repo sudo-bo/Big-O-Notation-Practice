e2e2dc11425921a38c331c5ce63716fddb9b0949
bo <bodrye.kam@gmail.com>
2023-11-01 21:54:59 -0400

    another exercise, another solution

--- app.js
+++ app.js
@@ -43,3 +43,22 @@ class Garage {
     }
   }
 }
+
+// How Web Works Exercise
+/*
+-http: a set of rules as for how data is sent and accessed to and from the server
+-url: adress for internet resources
+-dns: phonebook for urls, the internet resources
+-query string: a specifier for web pages that uses key value pairs
+-get and post: get simply requests information from the server, while post sends changes to the server
+-http request: when data is requested from a client to the server using html protocol, often the data requested is an html page
+-http response: the inverse of the above
+-http header: headers provide information about the request or the response
+-what happens when you type a URL in a browser?: a lot
+
+part2:
+-curl "https://icanhazdadjoke.com/search?term=pirate"
+-dig +short icanhazdadjoke.com
+
+
+*/
